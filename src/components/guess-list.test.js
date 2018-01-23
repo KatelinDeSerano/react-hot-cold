7fb97613fa0923f7674876e4cc5bae68276fb98a
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-01-23 14:57:24 -0600

    added tests to guess list

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -7,5 +7,14 @@ import GuessList from './guess-list';
 describe('<GuessList />', () => {
   it('Renders without crashing', () => {
     shallow(<GuessList guesses={[]} />);
-  })
+  });
+  it('Renders a list of guesses', () => {
+    const values = [23, 42, 88];
+    const wrapper = shallow(<GuessList guesses={values}/>);
+    const items = wrapper.find('li');
+    expect(items.length).toEqual(values.length);
+    values.forEach((value, index) => {
+      expect(items.at(index).text()).toEqual(value.toString());
+    });
+  });
 });
